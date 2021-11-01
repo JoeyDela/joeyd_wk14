d7961fb863d84324429fa205c246c5d04de907b1
JoeyDela <duffy.west87@gmail.com>
2021-11-01 18:37:33 -0500

    userdb

---
+++ models/user.js
@@ -0,0 +1,3 @@
+const { Model, DataTypes } = require("sequeliez");
+const bcrypt = require("bcrypt");
+const sequeliez = require("");
---
+++ server.js
@@ -0,0 +1,7 @@
+//Dependencies
+const express = require("express");
+const session = require("express-session");
+const routes = require("");
+//Set up express app
+//Set up routes
+//Start the server
